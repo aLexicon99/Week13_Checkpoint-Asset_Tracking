d31a4af484b7e6aca0645ae91714cb0c18c9ab2a
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-03-25 18:55:46 +0100

    info-files

---
+++ files/CurrencyConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace AssetTracker
+{
+    static class CurrencyConverter
+    {
+        static private string xmlUrl = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+        static Envelope envelope = CurrencyConverter.Update();
+
+
+        static public decimal ConvertTo(decimal value, string currency, out decimal newValue)
+        {
+            //If something is very wrong the returned value is -1
+            newValue = -1;
+
+            //Make USD currency input to EURO
+            foreach (var cube in envelope.Cube.Cube1.Cube)
+            {
+                if (cube.currency == "USD")
+                {
+                    newValue = value / cube.rate;
+                    break;
+                }
+            }
+
+            if (currency == "EUR")
+            {
+                return newValue;
+            }
+
+            //Find and convert to input currency
+            foreach (var cube in envelope.Cube.Cube1.Cube)
+            {
+                if (cube.currency == currency) 
+                {
+                    newValue *= cube.rate;
+                    break;
+                }
+            }
+
+            return newValue;
+        }
+
+
+        static public Envelope Update() 
+        {
+            try 
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+                XmlReader xmlReader = XmlReader.Create(xmlUrl);
+
+                using (xmlReader)
+                {
+                    envelope = (Envelope)(serializer.Deserialize(xmlReader));
+                }
+
+                return envelope;
+            } 
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+                return new Envelope();
+            }
+        }
+    }
+}
