3d62aef6096fc90a899b75a330ef9deca7a880fb
aLexicon99 <267594691+aLexicon99@users.noreply.github.com>
2026-03-25 14:33:10 +0100

    initial

---
+++ Week13_Asset-Tracking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week13_Asset_Tracking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
